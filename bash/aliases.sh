26d73e0a1183202f59fa758c3f41fb2b9f9d90cd
Dustin Garrett <dustin.garrett@stonecroptech.com>
2020-12-13 00:09:08 -0600

    adding alias for starting rails development enviroment in ssl

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -16,6 +16,7 @@ alias shrugcc='echo "¯\\\\\_(ツ)_/¯" | pbcopy'
 alias get_idf='. $HOME/esp/esp-idf/export.sh'
 alias badger='cd /Users/dustingarrett/projects/stonecroptech/badger/'
 alias homeauto='cd /Users/dustingarrett/projects/rails_framework'
+alias railsssl="rails s -b 'ssl://localhost:3000?key=config/local-certs/localhost-key.pem&cert=config/local-certs/localhost.pem'"
 
 alias irb="pry"
 alias pry="pry --simple-prompt"
